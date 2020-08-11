83dcbd7cc31b62b082338a735a2a78003660f9cd
Lizeth Toasa <lizeth.toasa@gmail.com>
2020-08-10 23:22:55 -0500

    Ingreso proyecto

---
+++ curriculum/src/app/tab2/tab2.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+  //num1: number;
+  //num2: number;
+  constructor() {}
+  /*mensaje(){
+    console.log(this.num1+this.num2);
+  }*/
+}
